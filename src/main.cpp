d478d28e6223ea070cd3ed37433174da9f7f1c47
Abdelaziz Hassan <abdelaziz.hassan.846@gmail.com>
2025-12-30 17:49:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include "cloud_manager.h"
 #include "firebase_manager.h" 
 
-// توقيتات النظام
 unsigned long lastSystemTick = 0;
 const long SYSTEM_TICK_INTERVAL = 2000;
 
@@ -14,8 +13,6 @@ unsigned long lastGoogleLog = 0;
 
 void setup() {
     Serial.begin(BAUD_RATE);
-    
-    // 1. تهيئة الأجهزة والاتصال
     initSensors();
     
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -26,7 +23,6 @@ void setup() {
     }
     Serial.println("\nConnected!");
 
-    // 2. تهيئة الخدمات السحابية
     initTelegram();
     initCloud(); 
     initFirebase();
@@ -43,24 +39,13 @@ void loop() {
         bool isFire = isFlameDetected();
         String flameStr = isFire ? "DETECTED" : "Safe";
 
-        // التحقق من صلاحية البيانات قبل إكمال العمليات (Error Handling)
-        if (isnan(temp) || isnan(hum)) {
-            Serial.println("Error: Failed to read from DHT sensor!");
-            return; // تخطي هذه الدورة إذا كانت القراءة خاطئة
-        }
-
         sendDataToFirebase(temp, hum, flameStr);
         checkSystemConditions(temp, hum, isFire);
 
-        // ج) تسجيل البيانات في Google Sheets
-        // هذا الجزء له توقيت خاص (أبطأ)، لذا نضعه داخل شرط فرعي
         if (currentMillis - lastGoogleLog >= LOG_INTERVAL) {
             logDataToGoogleSheet(temp, hum, isFire);
             lastGoogleLog = currentMillis;
         }
-        
-    
-     //   Serial.printf("Tick: T=%.1f, H=%.1f, Fire=%s\n", temp, hum, flameStr.c_str());
     }
 }
 
